13f8d4d994ca51bd69b7b5756b4781070dffee32
Philipp Wirtenberger <Philipp.Wirtenberger@student.uibk.ac.at>
2015-01-04 15:19:21 +0100

    simple db integration
    added simple database example
    uses local in-memory database (i.e. db is temporarily stored on the pc
    itself)

--- Project/app/controllers/Application.java
+++ Project/app/controllers/Application.java
@@ -1,14 +1,30 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
+import models.User;
+import play.*;
+import play.data.Form;
+import play.db.ebean.Model;
+import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    	//Query users from database
+    	List<User> users = new Model.Finder<String, User>(String.class, User.class).all();
+    	//Passing the list to the view
+        return ok(index.render(users));
+    }
+    
+    public static Result register() {
+    	//Retrieve data from POST
+    	User user = Form.form(User.class).bindFromRequest().get();
+    	//Save user into database
+    	user.save();
+    	
+    	return redirect(routes.Application.index());
     }
 
 }
---
+++ Project/app/models/User.java
@@ -0,0 +1,36 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import play.db.ebean.Model;
+
+@Entity
+public class User extends Model {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String email;
+	
+	private String password;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
