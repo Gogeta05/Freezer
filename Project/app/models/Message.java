d89f38bb9d10f8640603e3303578c7c0754955af
Philipp Wirtenberger <Philipp.Wirtenberger@student.uibk.ac.at>
2015-01-17 23:42:54 +0100

    added comments

--- Project/app/models/Message.java
+++ Project/app/models/Message.java
@@ -1,9 +1,21 @@
 package models;
 
+/**
+ * A message.
+ */
 public class Message {
 
+	/**
+	 * The author of the message
+	 */
 	public User from;
+	/**
+	 * The recipient.
+	 */
 	public User to;
+	/**
+	 * The message that is sent.
+	 */
 	public String msg;
 	
 	protected Message(User from, User to, String msg) {
@@ -12,6 +24,10 @@ public class Message {
 		this.msg = msg;
 	}
 	
+	/**
+	 * The reply to this message.
+	 * @param msg the content of the reply
+	 */
 	protected void reply(String msg) {
 		to.sendMsg(msg, from);
 	}
--- Project/app/models/MessageBox.java
+++ Project/app/models/MessageBox.java
@@ -2,16 +2,34 @@ package models;
 
 import java.util.ArrayList;
 
+/**
+ * The inbox of a User.
+ */
 public class MessageBox {
 	
+	/**
+	 * The messages currently in the inbox
+	 */
 	public ArrayList<Message> messages;
 	
+	/**
+	 * Deletes a message
+	 * @param msg the message to delete
+	 */
 	public void deleteMsg(Message msg) {
 		messages.remove(msg);
 	}
+	/**
+	 * Deletes all messages in the inbox.
+	 */
 	public void deleteAll() {
 		messages.clear();
 	}
+	/**
+	 * Sends a reply to a message.
+	 * @param newMsg	the reply
+	 * @param msg		the message to reply to
+	 */
 	public void reply(String newMsg, Message msg) {
 		msg.reply(newMsg);
 	}
