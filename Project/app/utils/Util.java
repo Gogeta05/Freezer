fb916c5331de03127e9366eb3e25389696db175e
MasterMoritz <greipl@hotmail.com>
2015-01-17 20:06:31 +0100

    Util.java now contains all URLs/paths
    Use them instead of directly implementing the String in your code.
    If a change to one of the links occurrs, it only has to be changed in the Util.java
    and the remaining code can be left untouched

--- Project/app/utils/Util.java
+++ Project/app/utils/Util.java
@@ -1,5 +1,7 @@
 package utils;
 
 public final class Util {
-
+	/* paths */
+	static public String url_LiftXls = "https://gis.tirol.gv.at/ogd/sport_freizeit/Aufstiegshilfen.xls";
+	static public String file_Interests = "/app/assets/Interests.xls";
 }
