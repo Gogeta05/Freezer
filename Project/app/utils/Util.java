a2ed7b984c0cff7d4dafb396707a6d8fae4ed2b0
MasterMoritz <greipl@hotmail.com>
2015-01-18 19:47:18 +0100

    change Interests.xls to relative path
    This was an absolute path before, hence the file was not found

--- Project/app/utils/Util.java
+++ Project/app/utils/Util.java
@@ -3,5 +3,5 @@ package utils;
 public final class Util {
 	/* paths */
 	static public String url_LiftXls = "https://gis.tirol.gv.at/ogd/sport_freizeit/Aufstiegshilfen.xls";
-	static public String file_Interests = "/app/assets/Interests.xls";
+	static public String file_Interests = "app/assets/Interests.xls";
 }
